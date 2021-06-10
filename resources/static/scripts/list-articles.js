96a0a102caaa37105a73b9832fd9b0dc501b5fd5
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2021-06-10 10:52:52 +0300

    add functionality for pagination

--- resources/static/scripts/list-articles.js
+++ resources/static/scripts/list-articles.js
@@ -1,13 +1,16 @@
+if(window.location.href.includes("/category/")){
+    let limitValue = window.location.href.split("?")[1].split("&")[0].split("=")[1];
+
+    let selectBox = document.getElementById(`article-limit-option-${limitValue}`);
+
+    selectBox.setAttribute("selected", "selected");
+}
+
 function changeLimit(){
     let selectBox = document.getElementById("article-limit");
     let selectedValue = selectBox.options[selectBox.selectedIndex].value;
 
-    let tempArray = window.location.href.split("?");
-    let searchParameters = tempArray[1];
-
-    tempArray = searchParameters.split("&");
-
-    let limit = tempArray[0];
+    let limit = window.location.href.split("?")[1].split("&")[0];
 
     if(limit.localeCompare(`limit=${selectedValue}`) !== 0)
         window.location.href = window.location.href.replace(limit, `limit=${selectedValue}`);
